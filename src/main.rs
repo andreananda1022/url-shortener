da3425ba26a14e603ac7f895032208b54f579672
andreananda1022 <andreananda1022@gmail.com>
2026-09-08 10:38:18 +0700

    feat: add initial async HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+async fn root_handler() -> &'static str {
+    "Hello, URL Shortener!"
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let app = Router::new().route("/", get(root_handler));
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
