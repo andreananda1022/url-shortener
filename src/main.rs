c1d7c0eaafcf2b5bc6a9a0bd73a1c7b6dc9e5782
andreananda1022 <andreananda1022@gmail.com>
2026-09-11 08:34:59 +0700

    feat(database): add initial PostgreSQL connection setup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
-use axum::{Router, routing::get};
+use axum::{Router, extract::State, routing::get};
+use sqlx::postgres::PgPoolOptions;
+use std::sync::Arc;
 use tokio::net::TcpListener;
+
+#[derive(Clone)]
+struct AppState {
+    db_pool: sqlx::PgPool,
+}
 
 async fn root_handler() -> &'static str {
     "Hello, URL Shortener!"
 }
 
+async fn health_check(State(state): State<Arc<AppState>>) -> &'static str {
+    sqlx::query!("SELECT 1 as one")
+        .fetch_one(&state.db_pool)
+        .await
+        .unwrap();
+    "Database is connected!"
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root_handler));
+    dotenvy::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL harus di-set di .env");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .unwrap();
+
+    let app_state = Arc::new(AppState { db_pool: pool });
+
+    let app = Router::new()
+        .route("/", get(root_handler))
+        .route("/health", get(health_check))
+        .with_state(app_state);
+
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
