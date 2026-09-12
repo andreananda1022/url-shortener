82b70c3717a3168255b3d9e975ba9f895016fad0
andreananda1022 <andreananda1022@gmail.com>
2026-09-12 20:54:07 +0700

    feat: integrate Redis and add connection check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{Router, extract::State, routing::get};
+use redis::AsyncCommands;
 use sqlx::postgres::PgPoolOptions;
 use std::sync::Arc;
 use tokio::net::TcpListener;
@@ -6,6 +7,7 @@ use tokio::net::TcpListener;
 #[derive(Clone)]
 struct AppState {
     db_pool: sqlx::PgPool,
+    redis_client: redis::aio::ConnectionManager,
 }
 
 async fn root_handler() -> &'static str {
@@ -20,10 +22,24 @@ async fn health_check(State(state): State<Arc<AppState>>) -> &'static str {
     "Database is connected!"
 }
 
+async fn redis_check(State(state): State<Arc<AppState>>) -> String {
+    let mut client = state.redis_client.clone();
+    let _: () = client
+        .set("ping", "pong")
+        .await
+        .unwrap();
+    client.get("ping").await.unwrap()
+}
+
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL harus di-set di .env");
+    let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL harus di-set di .env");
+    let client = redis::Client::open(redis_url).expect("Gagal menginisialisasi klien!");
+    let connection_manager = redis::aio::ConnectionManager::new(client)
+        .await
+        .expect("Gagal membuat connection manager Redis!");
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -31,11 +47,15 @@ async fn main() {
         .await
         .unwrap();
 
-    let app_state = Arc::new(AppState { db_pool: pool });
+    let app_state = Arc::new(AppState {
+        db_pool: pool,
+        redis_client: connection_manager,
+    });
 
     let app = Router::new()
         .route("/", get(root_handler))
         .route("/health", get(health_check))
+        .route("/redis-check", get(redis_check))
         .with_state(app_state);
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
